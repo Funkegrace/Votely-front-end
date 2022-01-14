7d2f5a3c4cdc5525c13fcd1a52d08c3908893b22
Funke Olanrewaju <olanrewajufunke28@gmail.com>
2022-01-14 13:32:41 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+// import * as LogRocket from 'logrocket';
+// LogRocket.init('osuhfs/lendsqr');
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'votely';
+}
